23217ee23400ebc82ca4f1663c0050ab9b6a2868
Kevin Emunds <kevin.emunds_1@student.hs-rm.de>
2024-10-30 16:40:35 +0100

    new build

---
+++ assets/WeekNavigationLayout.7ab82ab8.js
@@ -0,0 +1,1 @@
+import{_ as o,Y as r,N as t,O as n}from"./index.c1f9f1e7.js";const a={};function c(_,s){const e=r("router-view");return t(),n(e)}var f=o(a,[["render",c]]);export{f as default};
--- assets/WeekNavigationLayout.ae839ade.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,Y as r,N as t,O as n}from"./index.cc3dff0e.js";const a={};function c(_,s){const e=r("router-view");return t(),n(e)}var f=o(a,[["render",c]]);export{f as default};
